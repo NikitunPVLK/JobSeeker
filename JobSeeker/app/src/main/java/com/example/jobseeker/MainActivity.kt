28200ebd00eea6ee6a010d311cdc0e0ec7986a74
NikitunPVLK <nikitunpvlk@gmail.com>
2023-06-06 18:52:19 +0300

    Back navigation implemented

--- JobSeeker/app/src/main/java/com/example/jobseeker/MainActivity.kt
+++ JobSeeker/app/src/main/java/com/example/jobseeker/MainActivity.kt
@@ -2,18 +2,25 @@ package com.example.jobseeker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.jobseeker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
 
         setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
